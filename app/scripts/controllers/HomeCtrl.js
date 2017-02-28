2212dc11d3c2c865d05ecbd9e4b1b5d299f8bc30
Remington Stone <remstone7@gmail.com>
2017-02-27 20:25:08 -0500

    delete rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -10,12 +10,18 @@
                 size: 'md'
             });
         };
+        
+        // remove chat room
+        $scope.delete = function($index){
+
+            $scope.rooms.$remove($index);
+        };
+        
+        
+        
         return $scope.rooms
     };
     
-//    track by index
-//    ng-repeat track by index
-//    ng-click and pass in the getCurrentIndex
 
     angular
         .module('blocChat')
